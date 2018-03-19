a81f1ca88251c7d67ee40f063989e74061d512b6
Ove Sundal <osu002@uit.no>
2018-03-19 16:38:58 +0100

    add ErrorLoggerTests

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        private ErrorLogger _logger;
+
+        //called before each test
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new ErrorLogger();
+        }
+
+        [Test]
+        //testing void methods
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            _logger.Log("a");
+
+            Assert.That(_logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        //testing exceptions
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            Assert.That(() => _logger.Log(error), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        //testing events
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var id = Guid.Empty;
+            _logger.ErrorLogged += (sender, args) => { id = args; };
+            
+            //if error logged, id will no longer be empty
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
+    }
+}
