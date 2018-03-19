0ec8d4e329859437a40ce3cd9a40eecef648e5e0
Ove Sundal <osu002@uit.no>
2018-03-19 14:19:49 +0100

    add tests to Math class GetOddNumbers

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
@@ -14,26 +15,47 @@ namespace TestNinja.UnitTests
         {
             _math = new Math();
         }
-        
+
         [Test]
         //ignore a test
         //[Ignore("Because i wanted to!")]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
             var result = _math.Add(1, 2);
-            
+
             Assert.That(result, Is.EqualTo(3));
         }
+
         [Test]
         //parametrized test
-        [TestCase(2,1,2)]
-        [TestCase(1,2,2)]
-        [TestCase(1,1,1)]
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
         public void Max_WhenCalled_ReturnsTheGreaterArgument(int a, int b, int expectedResult)
         {
             var result = _math.Max(a, b);
-            
+
             Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnsOddNumbersUpToLimit()
+        {
+            var result = _math.GetOddNumbers(5);
+
+//            Assert.That(result, Is.Not.Empty);
+//            
+//            Assert.That(result.Count(), Is.EqualTo(3));
+//            
+//            Assert.That(result, Is.Ordered);
+//            
+//            Assert.That(result, Is.Unique);
+//            
+//            Assert.That(result, Does.Contain(1));
+//            Assert.That(result, Does.Contain(3));
+//            Assert.That(result, Does.Contain(5));
+
+            Assert.That(result, Is.EquivalentTo(new[] {1, 3, 5}));
         }
     }
 }
