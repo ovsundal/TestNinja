354d4a421880eec76255e3502735563f4560e1ff
Ove Sundal <osu002@uit.no>
2018-03-20 04:53:17 +0100

    add FizzBuzzTests

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+   
+        [Test]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        public void GetOutput_NumberIsDivisibleBy3AndOr5_ShouldReturnCorrectString(int a, string expectedResult)
+        {
+            var result = FizzBuzz.GetOutput(a);
+            
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+        
+        [Test]
+        public void GetOutput_NumberIsNotDivisibleBy3Or5_ShouldReturnSameNumberAsString()
+        {
+            var result = FizzBuzz.GetOutput(1);
+            
+            Assert.That(result, Is.EqualTo("1"));
+        }
+    }
+}
