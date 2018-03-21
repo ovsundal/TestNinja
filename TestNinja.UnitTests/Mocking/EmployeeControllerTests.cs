d5b5f374b3db27fa67c2d545bc8580548bff7d84
Ove Sundal <osu002@uit.no>
2018-03-21 10:46:27 +0100

    add EmployeeControllerTests

---
+++ TestNinja.UnitTests/Mocking/EmployeeControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class EmployeeControllerTests
+    {
+        private Mock<IEmployeeStorage> _employeeStorage;
+        private EmployeeController _employeeController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _employeeStorage = new Mock<IEmployeeStorage>();
+            _employeeController = new EmployeeController(_employeeStorage.Object);
+        }
+        
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeletesEmployeeFromDatabase()
+        {
+            _employeeStorage.Setup(es => es.DeleteEmployee(1));
+
+            //don't care about the result, this is an interaction test
+            _employeeController.DeleteEmployee(1);
+            
+            //verifies that the method was called and parameter 1 was passed
+            _employeeStorage.Verify(es => es.DeleteEmployee(1));
+        }
+    }
+}
---
+++ TestNinja/Mocking/EmployeeStorage.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.Entity;
+
+namespace TestNinja.Mocking
+{
+    public interface IEmployeeStorage
+    {
+        void DeleteEmployee(int id);
+    }
+
+//calling a db touches an external resource - refactor outside method
+    public class EmployeeStorage : IEmployeeStorage
+    {
+        private EmployeeContext _db;
+
+        public EmployeeStorage()
+        {
+            _db = new EmployeeContext();
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            var employee = _db.Employees.Find(id);
+            if (employee == null) return;
+            _db.Employees.Remove(employee);
+            _db.SaveChanges();
+        }
+    }
+
+    public class EmployeeContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public void SaveChanges()
+        {
+        }
+    }
+
+    public class Employee
+    {
+    }
+}
